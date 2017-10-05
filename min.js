5e35595967a151e460a5604602175b6cea3feea6
Benjamin Rosner <brosner@barnard.edu>
2017-10-05 11:11:05 -0400

    No need for units.

--- min.js
+++ min.js
@@ -3,6 +3,6 @@
   $('.tool_content_wrapper iframe').css({'height': desiredHeight, 'width': '100%'});
   // make this timer, loop until finally set
   setTimeout( function() {
-    parent.postMessage(JSON.stringify({subject: 'lti.frameResize', height: desiredHeight+"px"}), '*');
+    parent.postMessage(JSON.stringify({subject: 'lti.frameResize', height: desiredHeight}), '*');
   }, 2000);
 })( jQuery );
