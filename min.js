48e1544abfc53f0df2aa7b7951e0eaf114aa0d24
Benjamin Rosner <brosner@barnard.edu>
2017-10-05 11:09:52 -0400

    init upload
    Initial.

---
+++ min.js
@@ -0,0 +1,8 @@
+(function ( $ ) {
+  var desiredHeight = $(window).height();
+  $('.tool_content_wrapper iframe').css({'height': desiredHeight, 'width': '100%'});
+  // make this timer, loop until finally set
+  setTimeout( function() {
+    parent.postMessage(JSON.stringify({subject: 'lti.frameResize', height: desiredHeight+"px"}), '*');
+  }, 2000);
+})( jQuery );
